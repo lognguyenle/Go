28e6155a2cd5d1266b0a5c0f7ffd2b0313c10324
lognguyenle <longnguyenle05@gmail.com>
2024-03-18 15:25:19 -0700

    button class, separate click events

--- menu_scene/main.py
+++ menu_scene/main.py
@@ -11,26 +11,57 @@ FPS = 60
 WINDOWHEIGHT = 720
 WINDOWWIDTH = 1280
 
+ANTIALLIASING = True
+
 BGCOLOR = WHITE
 
 class WindowTypes:
     one = "menu"
     two = "game"
+    
+class UIButton:
+    def __init__(self, text, color, highlightColor, fontObj, pos):
+        self.pos = pos
+        self.fontObj = fontObj
+        self.text = text
+        self.textColor = color
+        self.highlightColor = highlightColor
+        self.buttonObj = self.fontObj.render(self.text, ANTIALLIASING, self.textColor, WHITE)
+        self.buttonRectObj = self.buttonObj.get_rect()
+        self.buttonRectObj.topleft = (pos[0], pos[1])
+        
+    def getBlitObj(self):
+        return (self.buttonObj, self.buttonRectObj)
+        
+    def clicked(self, mousex, mousey, mouseClicked):
+        if self.buttonRectObj.collidepoint((mousex, mousey)):
+            self.buttonObj = self.fontObj.render(self.text, ANTIALLIASING, self.textColor, self.highlightColor)
+            if mouseClicked:
+                return True
+        else: 
+            self.buttonObj = self.fontObj.render(self.text, ANTIALLIASING, self.textColor, WHITE)
+        return False
+        
+    
 
 def main():
     pygame.init()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-    
+
     mousex = 0
     mousey = 0
     pygame.display.set_caption('Python Go!')
-    
-    blitList = main_menu(YELLOW)
+
+    buttonList = main_menu()
     current = WindowTypes.one
-    
+    blitList = []
+
     while True: 
         mouseClicked = False
-        
+        blitList = []
+        for button in buttonList:
+            blitList.append(button.getBlitObj())
+
         DISPLAYSURF.fill(WHITE)
         DISPLAYSURF.blits(blitList)
         for event in pygame.event.get():
@@ -42,53 +73,38 @@ def main():
             elif event.type == MOUSEBUTTONUP:
                 mousex, mousey = event.pos
                 mouseClicked = True
-        
+
         match current:
             case WindowTypes.one:
-                if over_menu_button(mousex, mousey, blitList):
-                    blitList = main_menu(GREEN)
-                else:
-                    blitList = main_menu(YELLOW)
-                    ##
-            # case WINDOWTYPES[1]:
-            #     if over
-        
+                for button in buttonList:
+                    if button.clicked(mousex, mousey, mouseClicked):
+                        print('CLICKED')
+
         pygame.display.update()
     
-def main_menu(highlight): 
+def main_menu(): 
     objList = []
     
     fontObj = pygame.font.SysFont('BELL.TTF', 32)
     titleFontObj = pygame.font.Font('Ojuju-VariableFont_wght.ttf', 50)
 
-    titleTextObj = titleFontObj.render('Go', True, (0,0,0), highlight)
-    titleTextRectObj = titleTextObj.get_rect()
-    titleTextRectObj.topleft = ( (WINDOWWIDTH/2) - (titleTextObj.get_width()/2), WINDOWHEIGHT/3)
-    objList.append((titleTextObj, titleTextRectObj))
-
-    startButtonObj = fontObj.render('Start', True, (0,0,0), highlight)
-    startButtonRectObj = startButtonObj.get_rect()
-    startButtonRectObj.topleft = ( (WINDOWWIDTH/2) - (startButtonObj.get_width()/2), WINDOWHEIGHT/3 + titleTextObj.get_height() + 10 + 50)
-    objList.append((startButtonObj, startButtonRectObj))
+    titleText = UIButton('Go', BLACK, YELLOW, titleFontObj, (WINDOWWIDTH/2, WINDOWHEIGHT/3))
+    objList.append(titleText)
     
-    optionsButtonObj = fontObj.render('Options', True, (0,0,0), highlight)
-    optionsButtonRectObj = optionsButtonObj.get_rect()
-    optionsButtonRectObj.x
-    optionsButtonRectObj.topleft = ( (WINDOWWIDTH/2) - (optionsButtonObj.get_width()/2), WINDOWHEIGHT/3 + titleTextObj.get_height() + startButtonObj.get_height() + 10 + 50 + 50)
-    objList.append((optionsButtonObj, optionsButtonRectObj))
+    playButton = UIButton('Play', BLACK, YELLOW, titleFontObj, (WINDOWWIDTH/2, titleText.buttonRectObj.bottom))
+    objList.append(playButton)
+    
+    settingsButton = UIButton('Settings', BLACK, YELLOW, titleFontObj, (WINDOWWIDTH/2, playButton.buttonRectObj.bottom))
+    objList.append(settingsButton)
+    
+    quitButton = UIButton('Quit', BLACK, YELLOW, titleFontObj, (WINDOWWIDTH/2, settingsButton.buttonRectObj.bottom))
+    objList.append(quitButton)
     
     return objList
 
-def over_menu_button(x, y, objList):
-    for obj in objList:
+# def main_menu_clicked(current, objList, mousex, mousey, mouseClicked):
+#     for 
         
-        if (x >= obj[1].topleft[0]) and (x <= obj[1].topleft[0] + obj[1].width) and (y >= obj[1].topleft[1]) and (y <= obj[1].topleft[1]+(obj[1].height)):
-            return True
-    return False
-        
-        
-    
-
 # def game_menu():
 #     a
 
