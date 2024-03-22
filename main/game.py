2ec0a5df3bced08ce6eaba6cf437ebf6a9dfe5e3
lognguyenle <longnguyenle05@gmail.com>
2024-03-22 00:32:54 -0700

    game object

---
+++ main/game.py
@@ -0,0 +1,36 @@
+import pygame
+import constants as const
+import ui_elements as ui
+import scene
+from pygame.locals import *
+
+def game_objs():
+    obj_list = []
+    
+    #boardObject
+    
+    
+    return obj_list
+
+def boardObject():
+    pass
+
+class GameScene(scene.BareScene):
+    def __init__(self):
+        self.next = self
+        self.obj_list = game_objs()
+        self.to_render = []
+        self.mousepos = (0, 0)
+        self.bgupdate = True
+    
+    def ProcessInput(self, events, pressed_keys):
+        mouse_clicked = False
+        for event in events:
+            if event.type == MOUSEMOTION:
+                self.mousepos = event.pos
+            elif event.type == MOUSEBUTTONUP:
+                self.mousepos = event.pos
+                mouse_clicked = True
+        for obj in self.obj_list:
+            obj.update(self.mousepos[0], self.mousepos[1], mouse_clicked)
+    
