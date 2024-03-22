c90b13d9b2253d7bb89666da982290b08ee07b5b
lognguyenle <longnguyenle05@gmail.com>
2024-03-22 00:33:43 -0700

    added a new BareScene class

--- main/scene.py
+++ main/scene.py
@@ -1,5 +1,7 @@
 #Referenced from https://nerdparadise.com/programming/pygame/part7
 
+import constants as const
+
 class Scene:
     def __init__(self):
         self.next = self
@@ -18,3 +20,21 @@ class Scene:
         
     def Terminate(self):
         self.SwitchToScene(None)
+        
+class BareScene(Scene):
+    def __init__(self):
+        self.next = self
+        self.obj_list = []
+        self.to_render = []
+        self.mousepos = (0, 0)
+        self.bgupdate = True
+    
+    def Update(self):
+        self.to_render = []
+        for obj in self.obj_list:
+            self.to_render.append(obj.get_blit_obj())
+        
+    def Render(self, screen):
+        if self.bgupdate:
+            screen.fill(const.BGCOLOR)
+        screen.blits(self.to_render)
