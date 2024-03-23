b9179b12ec2f63a8619905f5c5ac132972b9848a
lognguyenle <longnguyenle05@gmail.com>
2024-03-23 15:08:05 -0700

    scenes from game and menu moved here

--- main/scene.py
+++ main/scene.py
@@ -1,6 +1,9 @@
 #Referenced from https://nerdparadise.com/programming/pygame/part7
 
 import constants as const
+import main_menu as menu
+import game
+from pygame.locals import *
 
 class Scene:
     def __init__(self):
@@ -38,3 +41,57 @@ class BareScene(Scene):
         if self.bgupdate:
             screen.fill(const.BGCOLOR)
         screen.blits(self.to_render)
+        
+class MenuScene(BareScene):
+    def __init__(self):
+        super().__init__()
+        self.obj_list = menu.main_menu()
+    
+    def ProcessInput(self, events, pressed_keys):
+        mouse_clicked = False
+        for event in events:
+            if event.type == MOUSEMOTION:
+                self.mousepos = event.pos
+            elif event.type == MOUSEBUTTONUP:
+                self.mousepos = event.pos
+                mouse_clicked = True
+            if event.type == menu.SETTINGSEVENT:
+                self.SwitchToScene(SettingsScene())
+            if event.type == menu.PLAYEVENT:
+                self.SwitchToScene(GameScene())
+        for obj in self.obj_list:
+            obj.update(self.mousepos[0], self.mousepos[1], mouse_clicked)
+        
+class SettingsScene(BareScene):
+    def __init__(self):
+        super().__init__()
+        self.obj_list = menu.settings_menu()
+        
+    def ProcessInput(self, events, pressed_keys):
+        mouse_clicked = False
+        for event in events:
+            if event.type == MOUSEMOTION:
+                self.mousepos = event.pos
+            elif event.type == MOUSEBUTTONUP:
+                self.mousepos = event.pos
+                mouse_clicked = True
+            if event.type == menu.MENUEVENT:
+                self.SwitchToScene(MenuScene())
+        for obj in self.obj_list:
+            obj.update(self.mousepos[0], self.mousepos[1], mouse_clicked)
+            
+class GameScene(BareScene):
+    def __init__(self):
+        super().__init__()
+        self.obj_list = game.game_objs()
+    
+    def ProcessInput(self, events, pressed_keys):
+        mouse_clicked = False
+        for event in events:
+            if event.type == MOUSEMOTION:
+                self.mousepos = event.pos
+            elif event.type == MOUSEBUTTONUP:
+                self.mousepos = event.pos
+                mouse_clicked = True
+        for obj in self.obj_list:
+            obj.update(self.mousepos[0], self.mousepos[1], mouse_clicked)
